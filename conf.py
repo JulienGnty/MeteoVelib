7634b59622b6ac0eb24ea12a20d5f16ed9d5c148
Julien Genety <julien.genety@gmail.com>
2023-03-31 13:33:20 +0200

    Add conf file

---
+++ conf.py
@@ -0,0 +1,11 @@
+# Directory path of the project
+DIRPATH = "D:/Programmation/Python/projects/MeteoVelib/"
+
+# Key for downloading from OpenWeatherMap
+KEY = "aaa"
+
+# Latitude and Longitude of, approximately, center of Paris
+LAT, LON = (48.862582, 2.338709)
+
+# Limits used to classify docks occupation
+LIMITS = [5,25,75,95]
