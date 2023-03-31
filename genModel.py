c421ac788e1aaf90be46df1ade4f8f83dd61cd92
Julien Genety <julien.genety@gmail.com>
2023-03-31 13:03:26 +0200

    Update models generation with more data and more features

--- genModel.py
+++ genModel.py
@@ -10,45 +10,82 @@ import joblib
 
 import conf as cf
 
+FEAT = [
+    "temperature", 
+    "humidity", 
+    "wind_speed", 
+    "strike", 
+    "demonstration", 
+    "weekday",
+    "year",
+    "month",
+    "day", 
+    "hour",
+    "minute", 
+]
+TARGET = ["occupation_prct"]
+
+VERSION = "V2"
 
 def preProcessDataframe(df: object) -> object:
-    dfret = df.dropna(subset = [
-        "station_id", 
-        "lat", 
-        "lon", 
-        "occupation_prct", 
-        "occupation_class"
-    ]).copy()
+    """
+        Delete rows with null values on columns needed as features or target
+    """
+    dfret = df.dropna(subset = FEAT + TARGET).copy()
     
     return dfret
 
-def splitDataframeTrainTest(file_list: list, step = 1) -> tuple:
-    if len(file_list) > 14:
-        file_list = file_list[-14:]
+def splitDataframeTrainTest(file_list: list, test_days = 7, limit = 28, step = 1) -> tuple:
+    """
+        Split data train and data test in chronological order.
 
-    df_train = dst.getDataset(file_list[0][-14:-4]).iloc[0::step]
-    for file in file_list[1:(len(file_list) - 2)]:
+        Parameters:
+        - file_list: the paths list of files containing the data; file_list must be sorted in chronological order
+        - test_days: the latest days used as data test; other days will be used as data train
+        - limit, default to 28: select only the 'limit' last files
+        - step, default to 1: select only one row on 'step' rows
+    """
+    if len(file_list) > limit:
+        file_list = file_list[-limit:]
+
+    train_list = file_list[:(len(file_list)-test_days)]
+    test_list = file_list[-test_days:]
+
+    df_train = dst.getDataset(train_list[0][-14:-4], version = VERSION).iloc[0::step]
+    for file in train_list[1:]:
         df_train = pd.concat([
             df_train,
-            dst.getDataset(file[-14:-4]).iloc[0::step]
+            dst.getDataset(file[-14:-4], version = VERSION).iloc[0::step]
         ])
 
-    df_test = dst.getDataset(file_list[-1][-14:-4]).iloc[0::step]
+    df_test = dst.getDataset(test_list[0][-14:-4], version = VERSION).iloc[0::step]
+    for file in test_list[1:]:
+        df_test = pd.concat([
+            df_test,
+            dst.getDataset(file[-14:-4], version = VERSION).iloc[0::step]
+        ])
 
     return preProcessDataframe(df_train), preProcessDataframe(df_test)
     
 def generateOneModel(df_train: object, df_test :object, station_id: int) -> object:
+    """
+        Generate a predidction model based on GradientBoostingRegressor
+        Dump the model into a compressed file with joblib
+        Save model informations in a dict which is returned
+    """
 
-    feat = ["station_id", "lon", "lat", "year", "month", "day", "hour", "minute"]
-    target = ["occupation_prct"]
-
-    x_train = df_train[df_train['station_id'] == station_id][feat]
-    y_train = df_train[df_train['station_id'] == station_id][target]
+    x_train = df_train[df_train['station_id'] == station_id][FEAT]
+    y_train = df_train[df_train['station_id'] == station_id][TARGET]
     
-    x_test = df_test[df_test['station_id'] == station_id][feat]
-    y_test = df_test[df_test['station_id'] == station_id][target]
+    x_test = df_test[df_test['station_id'] == station_id][FEAT]
+    y_test = df_test[df_test['station_id'] == station_id][TARGET]
 
-    clf = GradientBoostingRegressor(random_state=0)
+    # Can't fit the model if data are empty
+    if x_train.empty or y_train.empty or x_test.empty or y_test.empty:
+        return 0
+
+    clf = GradientBoostingRegressor(random_state = 42)
+
     clf.fit(x_train, y_train.values.ravel())
 
     y_pred = clf.predict(x_test)
@@ -61,43 +98,39 @@ def generateOneModel(df_train: object, df_test :object, station_id: int) -> obje
         ])
 
     model_dict = {
-        "station_id": station_id,
         "file_name": "velib_model_" + str(station_id) + ".joblib.z",
         "mae": np.round(mean_absolute_error(y_test, y_pred), 2),
-        "feat": feat,
-        "target": target,
+        "feat": FEAT,
+        "target": TARGET,
         "feature_importance": feat_importance,
-        "data": {
-            "time": df_test[df_test['station_id'] == station_id].time.dt.strftime("%Y-%m-%d %H:%M:%s").tolist(),
-            "y_test": list(y_test["occupation_prct"]),
-            "y_pred": list(y_pred),
-        }
     }
 
-    #print("mae:", model_dict["mae"])
+    print("mae:", model_dict["mae"])
     
     joblib.dump((clf), cf.DIRPATH + "model/" + model_dict["file_name"])
 
     return model_dict
 
 def generateMultipleModel(count = 0, step = 1):
-    df_train, df_test = splitDataframeTrainTest(dst.listDatasets(), step)
+    """
+        Generate several model. Each model is linked to a specific station
+        If count, it takes 'count' random stations to generate model on it
+        Else, it generates models on all stations
+    """
+    df_train, df_test = splitDataframeTrainTest(dst.listDatasets(version = VERSION), test_days = 5, step = step)
 
     station_list = list(set(dst.getStationsInfo().station_id))
 
     if count:
         station_list = random.sample(list(set(df_train.station_id)), count)
 
-    model_list = []
+    model_list = {}
     for station in station_list:
-        model_list.append(generateOneModel(df_train, df_test, station))
+        model_list[str(station)] = generateOneModel(df_train, df_test, station)
 
     with open(cf.DIRPATH + "model/velib_model_list.json", "w") as outfile:
         json.dump(model_list, outfile)
 
 
 if __name__ == "__main__":
-    print(cf.DIRPATH)
-    print("test")
-    print(dst.listDatasets()[-1][-14:-4])
-    generateMultipleModel(count = 700)
+    generateMultipleModel()
